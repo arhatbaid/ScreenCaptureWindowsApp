3319140b7e437a1aeaf460f544758db505b0acf8
Arhat Baid <abaid@kabbage.com>
2020-02-18 19:28:37 -0600

    First commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,9 +3,6 @@ package sample;
 import com.sun.jna.Native;
 import com.sun.jna.platform.win32.User32;
 import com.sun.jna.platform.win32.WinDef;
-import fr.slaynash.communication.handlers.OrderedPacketHandler;
-import fr.slaynash.communication.rudp.RUDPClient;
-import fr.slaynash.communication.utils.NetUtils;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -16,29 +13,20 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.net.SocketTimeoutException;
-import java.net.UnknownHostException;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Main extends Application {
 
     private static final String APPLICATION_NAME = "kchrome.exe";
-    public static final InetAddress SERVER_HOST = NetUtils.getInternetAdress("localhost");
-    public static final int SERVER_PORT = 56448;
-
-    public static RUDPClient client;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         openDialog(primaryStage);
-        initServer();
         startCapturingScreen();
     }
 
-    private WinDef.RECT activeWindoInfo() {
+    private WinDef.RECT activeWindowInfo() {
         char[] buffer = new char[1024 * 2];
         WinDef.HWND hwnd = User32.INSTANCE.GetForegroundWindow();
         User32.INSTANCE.GetWindowText(hwnd, buffer, 1024);
@@ -50,37 +38,14 @@ public class Main extends Application {
         return rect;
     }
 
-    private void initServer() {
-        try {
-            client = new RUDPClient(SERVER_HOST, SERVER_PORT);
-            client.setPacketHandler(OrderedPacketHandler.class);
-            client.connect();
-        } catch (SocketException e) {
-            System.out.println("Cannot allow port for the client. Client can't be launched.");
-            System.exit(-1);
-        } catch (UnknownHostException e) {
-            System.out.println("Unknown host: " + SERVER_HOST);
-            System.exit(-1);
-        } catch (SocketTimeoutException e) {
-            System.out.println("Connection to " + SERVER_HOST + ":" + SERVER_PORT + " timed out.");
-        } catch (InstantiationException e) {
-        } //Given handler class can't be instantiated.
-        catch (IllegalAccessException e) {
-        } //Given handler class can't be accessed.
-        catch (IOException e) {
-        }
-    }
-
     private void startCapturingScreen() {
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
                 try {
                     if (isDesiredApplicationIsRunning()) {
-                        client.sendReliablePacket(takeScreenShot(activeWindoInfo())); //Send packet to the server
+                        takeScreenShot(activeWindowInfo());
                     }
-//                    client.sendPacket(); //Send packet to the server
-                    client.disconnect(); //Disconnect from server
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -130,7 +95,7 @@ public class Main extends Application {
 
     private void openDialog(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Phantom Eye");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
