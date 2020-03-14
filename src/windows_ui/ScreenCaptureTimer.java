952d6f7f3291cc5f457df8e7e6bde1db0fc6a8ee
Cray <arhatbaid.developer@gmail.com>
2020-03-13 21:56:45 -0500

    @durgesh Added UI

--- src/windows_ui/ScreenCaptureTimer.java
+++ src/windows_ui/ScreenCaptureTimer.java
@@ -1,13 +1,21 @@
-package sample;
+package windows_ui;
 
 import com.sun.jna.Native;
 import com.sun.jna.platform.win32.User32;
 import com.sun.jna.platform.win32.WinDef;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.collections.FXCollections;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import org.jutils.jprocesses.JProcesses;
+import org.jutils.jprocesses.model.ProcessInfo;
+
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -15,17 +23,107 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class ScreenCaptureTimer extends Application {
 
-    private static final String APPLICATION_NAME = "kchrome.exe";
+    private static List<String> arrProcesses = new ArrayList<>();
+    Stage window;
+
+    public static void main(String[] args) {
+        getTask();
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        openDialog(primaryStage);
-        startCapturingScreen();
+        window = primaryStage;
+        window.setTitle("Phantom Eye");
+
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(10);
+        grid.setHgap(10);
+
+        //project name
+        javafx.scene.control.Label Projlabel = new javafx.scene.control.Label("Project Name:");
+        GridPane.setConstraints(Projlabel, 0, 0);
+        javafx.scene.control.TextField projText = new javafx.scene.control.TextField();
+        projText.setPrefWidth(20);
+        GridPane.setConstraints(projText, 1, 0);
+        Projlabel.setStyle("-fx-text-fill: #ff9a16;");
+
+        //password
+        javafx.scene.control.Label password = new javafx.scene.control.Label("Password:");
+        GridPane.setConstraints(password, 0, 1);
+        password.setStyle("-fx-text-fill: #ff9a16;");
+        javafx.scene.control.TextField passtext = new javafx.scene.control.TextField();
+        GridPane.setConstraints(passtext, 1, 1);
+
+        //time
+        javafx.scene.control.Label time = new javafx.scene.control.Label("Frame rate (Time):");
+        GridPane.setConstraints(time, 0, 2);
+        time.setStyle("-fx-text-fill: #ff9a16;");
+        javafx.scene.control.TextField timetext = new javafx.scene.control.TextField();
+        GridPane.setConstraints(timetext, 1, 2);
+
+        //radio for cursor
+        RadioButton rb1 = new RadioButton("Yes");
+        GridPane.setConstraints(rb1, 1, 3);
+        rb1.setStyle("-fx-text-fill: #ff9a16;");
+        RadioButton rb2 = new RadioButton("No");
+        rb2.setStyle("-fx-text-fill: #ff9a16;");
+        GridPane.setConstraints(rb2, 1, 4);
+        javafx.scene.control.Label mouse = new javafx.scene.control.Label("Cursor Control:");
+        mouse.setStyle("-fx-text-fill: #ff9a16;");
+        GridPane.setConstraints(mouse, 0, 3);
+
+        // drop down list
+        javafx.scene.control.Label select = new javafx.scene.control.Label("Select Application:");
+        select.setStyle("-fx-text-fill: #ff9a16;");
+        GridPane.setConstraints(select, 0, 5);
+        ChoiceBox<String> choice = new ChoiceBox(FXCollections.observableArrayList(arrProcesses));
+        //  ChoiceBox<String> choice = new ChoiceBox<>();
+        //  ComboBox choice = new ComboBox(FXCollections.observableArrayList(Proces));
+//        choice.getItems().add("Ashutosh");
+        //  choice.getItems().addAll(Proces);
+        GridPane.setConstraints(choice, 1, 5);
+        choice.getSelectionModel().selectedItemProperty().addListener((v, oldvalue, newvalue) -> System.out.println(newvalue));
+
+        //start button
+        javafx.scene.control.Button button = new Button("Start");
+        button.setStyle("-fx-text-fill: green;");
+
+        // button.setOnAction(e -> getChoice(choice));
+        button.setOnAction(e -> startCapturingScreen());
+
+        ToggleGroup radio = new ToggleGroup();
+        rb1.setToggleGroup(radio);
+        rb2.setToggleGroup(radio);
+
+        GridPane.setConstraints(button, 1, 7);
+        grid.getChildren().addAll(Projlabel, projText, password, passtext, time, timetext, mouse, rb1, rb2, select, button, choice);
+        Scene scene = new Scene(grid, 650, 270);
+        scene.getStylesheets().add("css/phantom.css");
+        window.setResizable(false);
+        window.setScene(scene);
+        window.show();
+
+    }
+
+
+
+
+
+    private static void getTask() {
+        List<ProcessInfo> processesList = JProcesses.getProcessList();
+
+        for (final ProcessInfo processInfo : processesList) {
+            arrProcesses.add(processInfo.getName().trim());
+        }
     }
 
     private WinDef.RECT activeWindowInfo() {
@@ -39,6 +137,18 @@ public class ScreenCaptureTimer extends Application {
         System.out.println("\n===========================================");
         return rect;
     }
+   /* private boolean isDesiredApplicationIsRunning() throws IOException {
+        String line;
+        StringBuilder pidInfo = new StringBuilder();
+        Process p = Runtime.getRuntime().exec(System.getenv("windir") + "\\system32\\" + "tasklist.exe");
+        BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        while ((line = input.readLine()) != null) {
+            pidInfo.append(line);
+        }
+        input.close();
+        return pidInfo.toString().toLowerCase().contains(APPLICATION_NAME);
+    }
+*/
 
     private void takeScreenShot(WinDef.RECT rect) {
         try {
@@ -81,41 +191,6 @@ public class ScreenCaptureTimer extends Application {
         }
     }
 
-    private void openDialog(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Phantom Eye");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-   /* private boolean isDesiredApplicationIsRunning() throws IOException {
-        String line;
-        StringBuilder pidInfo = new StringBuilder();
-        Process p = Runtime.getRuntime().exec(System.getenv("windir") + "\\system32\\" + "tasklist.exe");
-        BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        while ((line = input.readLine()) != null) {
-            pidInfo.append(line);
-        }
-        input.close();
-        return pidInfo.toString().toLowerCase().contains(APPLICATION_NAME);
-    }
-*/
-
-    private void startCapturingScreen() {
-        TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
-                try {
-                    takeScreenShot(activeWindowInfo());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        Timer timer = new Timer();
-        long delay = 1000L;
-        timer.scheduleAtFixedRate(task, delay, delay);
-    }
-
     /*private byte[] takeScreenShot(WinDef.RECT rect) {
         byte[] empty = new byte[0];
         try {
@@ -141,8 +216,19 @@ public class ScreenCaptureTimer extends Application {
         return empty;
     }*/
 
-
-    public static void main(String[] args) {
-        launch(args);
+    private void startCapturingScreen() {
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    takeScreenShot(activeWindowInfo());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        Timer timer = new Timer();
+        long delay = 1000L;
+        timer.scheduleAtFixedRate(task, delay, delay);
     }
 }
